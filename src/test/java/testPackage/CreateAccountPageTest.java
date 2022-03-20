1ac3fcde45c0b393735345ab267eec6b7da771c5
Adham Salem <adhamgsalem@gmail.com>
2022-03-20 18:05:59 +0000

    Adding testng.xml.

---
+++ src/test/java/testPackage/CreateAccountPageTest.java
@@ -0,0 +1,73 @@
+package testPackage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.BaseClass;
+import pages.CreateAccountPage;
+import pages.LandingPage;
+
+public class CreateAccountPageTest extends BaseClass {
+	
+	@Test(priority = 2, description = "Registration Page | Check that registration page is displayed ")
+	public void validateRegistrationPageIsDisplayed() {
+		LandingPage landingPage = new LandingPage(driver);
+		landingPage.clickAccountIcon();
+		landingPage.clickRegisterBtn();
+		CreateAccountPage registrationPage = new CreateAccountPage(driver);
+		Assert.assertTrue(registrationPage.geCreateAccounteMsg());
+	}
+	
+	@Test(priority = 3, description = "Registration Page | Check that warning message is displayed when user misses to input a value in a mandatory field")
+	public void validateMandatoryFieldsOnRegistration() {
+		LandingPage landingPage = new LandingPage(driver);
+		landingPage.clickAccountIcon();
+		landingPage.clickRegisterBtn();
+		CreateAccountPage registrationPage = new CreateAccountPage(driver);
+		registrationPage.clickRegisterBtn();
+		Assert.assertTrue(registrationPage.firstNameRequiredMsgDisplayed());
+		Assert.assertTrue(registrationPage.lastNameRequiredMsgDisplayed());
+		Assert.assertTrue(registrationPage.emailAddressRequiredMsgDisplayed());
+		Assert.assertTrue(registrationPage.passwordConfirmationRequiredMsgDisplayed());
+		Assert.assertTrue(registrationPage.passwordRequiredMsgDisplayed());
+	}
+	
+	@Test(priority = 4, description = "Registration Page | Check that warning message is displayed when user inputs pswd1 different from pswd2")
+	public void validatePasswordMatchingOnRegistration() {
+		LandingPage landingPage = new LandingPage(driver);
+		landingPage.clickAccountIcon();
+		landingPage.clickRegisterBtn();
+		CreateAccountPage registrationPage = new CreateAccountPage(driver);
+		registrationPage.typePassword();
+		registrationPage.typePasswordConfirmationWrong();
+		registrationPage.clickRegisterBtn();
+		Assert.assertTrue(registrationPage.passwordDoesNotMatchMsgDisplayed());
+	}
+	
+	@Test(priority = 5, description = "Registration Page | Check that warning message is displayed when user inputs a weak password")
+	public void validatePasswordComplexityOnRegistration() {
+		LandingPage landingPage = new LandingPage(driver);
+		landingPage.clickAccountIcon();
+		landingPage.clickRegisterBtn();
+		CreateAccountPage registrationPage = new CreateAccountPage(driver);
+		registrationPage.typePasswordIncorrect();
+		registrationPage.clickRegisterBtn();
+		Assert.assertTrue(registrationPage.passwordComplixtyMsgDisplayed());
+	}
+	
+	@Test(priority = 6, description = "Registration Page | Check that warning message is displayed when user inputs a weak password")
+	public void validateValidRegistration() {
+		LandingPage landingPage = new LandingPage(driver);
+		landingPage.clickAccountIcon();
+		landingPage.clickRegisterBtn();
+		CreateAccountPage registrationPage = new CreateAccountPage(driver);
+		registrationPage.typeFirstName();
+		registrationPage.typeMiddleName();
+		registrationPage.typeLastName();
+		registrationPage.typeEmailAddress();
+		registrationPage.typePassword();
+		registrationPage.typePasswordConfirmation();
+		registrationPage.subscripeToNewsLetter();
+		registrationPage.clickRegisterBtn();
+		Assert.assertTrue(registrationPage.//addEelmentOnPage());
+	}
+
+}
---
+++ src/test/java/testPackage/LandingPageTest.java
@@ -0,0 +1,15 @@
+package testPackage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.BaseClass;
+import pages.LandingPage;
+
+
+public class LandingPageTest extends BaseClass {
+	
+	@Test(priority = 1, description = "Landing Page | Check that landing page is displayed after navigating to the website")
+	public void validateLandingPageIsDisplayed() {
+		LandingPage landingPage = new LandingPage(driver);
+		Assert.assertTrue(landingPage.getWelcomeMsg());
+	}
+}
